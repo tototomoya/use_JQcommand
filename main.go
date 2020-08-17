4f34712cbbcd8cb4f6b6b170f686c1ea0f056d97
Tomoya <57219242+tototomoya@users.noreply.github.com>
2020-08-17 14:49:33 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,86 +1,88 @@
+// apt -y install jq
 package main
 
 import (
 	"encoding/gob"
-	"io/ioutil"
-	"os/exec"
-	"fmt"
-	"net/http"
-	"time"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os/exec"
+	"strconv"
+
+	"github.com/uniplaces/carbon"
+
 	"github.com/gorilla/mux"
 	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
-	"strconv"
 )
 
 var store = sessions.NewCookieStore(securecookie.GenerateRandomKey(32))
+var json_file = "diary.json"
 
 type Diary struct {
-	Date string
+	Date    string
 	Content string
 }
 
 type Jsontext struct {
 	User_name string
-	Date string
-	Content string
+	Date      string
+	Content   string
 }
-
 
 func write_json(data Jsontext) {
 	jsondata, _ := json.Marshal(data)
-	s := fmt.Sprintf("echo '%s' >> test.json", string(jsondata))
+	s := fmt.Sprintf("echo '%s' >> "+json_file, string(jsondata))
 	_ = exec.Command("bash", "-c", s).Run()
 }
 
-func write_DateContent(date string) {
-	s := "'. | select(.Date == " + strconv.Quote(date) + ") | select(.User_name == " + strconv.Quote("tomoya") + ") | .Content'"
-	s = "jq " + s + " test.json"
-	jq := exec.Command("bash", "-c", s)
-	out, _ := jq.Output()
-	_ = ioutil.WriteFile(date + "_tomoya", out, 0777)
+func write_JsonPerUser(name string, date string) {
+	cmd := "'. | select(.Date == " + strconv.Quote(date) + ") | select(.User_name == " + strconv.Quote(name) + ") | .Content'"
+	cmd = "jq " + cmd + " " + json_file
+	jq := exec.Command("bash", "-c", cmd)
+	jq_out, _ := jq.Output()
+	_ = ioutil.WriteFile(date+name, jq_out, 0777)
 }
 
 func createSession(w http.ResponseWriter, r *http.Request) {
 	gob.Register([]Diary{})
 
 	vars := mux.Vars(r)
-
-	now := time.Now()
-	nowUTC := now.UTC()
-	jst := time.FixedZone("Japan", 9*60*60)
-	nowJst := nowUTC.In(jst)
-	s := fmt.Sprintf("%d年%d月%d日", nowJst.Year(), int(nowJst.Month()), nowJst.Day())
+	content := vars["content"]
+	now, _ := carbon.NowInLocation("Asia/Tokyo")
+	s_now := now.DateTimeString()
 
 	var diarys []Diary
 	session, _ := store.Get(r, "getDiary")
-	if session.Values["diary"] != nil {
-	diarys = session.Values["diary"].([]Diary)
-	diarys = append(diarys, Diary{s, vars["content"]})
+	if session.Values["diarys"] != nil {
+		diarys = session.Values["diarys"].([]Diary)
+		diarys = append(diarys, Diary{s_now, content})
 	} else {
-	diarys = append(diarys, Diary{s, vars["content"]})
+		diarys = append(diarys, Diary{s_now, content})
 	}
 	session.Values["diarys"] = diarys
+	session.Values["name"] = vars["name"]
 	session.Save(r, w)
 
-	ss := fmt.Sprintf("cookieに%vを保存しました。", session.Values["diarys"])
-	fmt.Fprint(w, ss)
+	message := fmt.Sprintf("%vさんのcookieに%vを保存しました。", session.Values["name"], session.Values["diarys"])
+	fmt.Fprint(w, message)
 
 	go func() {
-		write_json(Jsontext{ vars["name"], s, vars["content"] } )
-		write_DateContent(s)
+		write_json(Jsontext{vars["name"], s_now, vars["content"]})
+		write_JsonPerUser(vars["name"], s_now)
 	}()
 }
 
 func getSession(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "getDiarys")
-	fmt.Fprint(w, session.Values["diarys"])
+	session, _ := store.Get(r, "getDiary")
+	message := fmt.Sprintf("%vさんのcookieのデータは%v ", session.Values["name"], session.Values["diarys"])
+	fmt.Fprint(w, message)
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/diary/{name}/{content}", createSession)
-	r.HandleFunc("/index/{name}", getSession)
-	http.ListenAndServe("0.0.0.0:8000", r)
+	r.HandleFunc("/list", getSession)
+	http.ListenAndServe("0.0.0.0:8001", r)
 }
